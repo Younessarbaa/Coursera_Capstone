13fd4954fa670cd3b72b24acd429cbfe563a5c18
Younessarbaa <53969532+Younessarbaa@users.noreply.github.com>
2019-08-10 16:42:27 +0200

    Update and rename README.md to Python_code.py

---
+++ Python_code.py
@@ -0,0 +1,5 @@
+# Coursera_Capstone
+This repo is for coursera capstone
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course')
--- README.md
+++
@@ -1,2 +0,0 @@
-# Coursera_Capstone
-This repo is for coursera capstone
